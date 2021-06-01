bf052fa1109afa59688c8d5c4a5632e987dc1ee7
Anthony Catelle <52525732+Tahxyonn@users.noreply.github.com>
2021-06-01 09:22:00 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,20 @@
 package com.example;
-
-
+/**
+ * My <b>Greeter</b>.
+ * Fonctionnalités pour les salutations
+ */
 public class Greeter {
-
-  
+  /**
+  * My <b>constructor</b>.
+  * default constructor
+  */
   public Greeter() {
-
   }
-
+  /**
+  * * My <b>method greet</b>.
+  * @param someone String
+  * @return String
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
